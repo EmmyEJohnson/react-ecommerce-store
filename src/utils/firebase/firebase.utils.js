e1b6110d5d7dd6842a155d0722d3ca84942e3362
Emily Johnson <emelizajo22@gmail.com>
2023-03-08 21:23:07 -0800

    create user docs and check firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,11 +4,13 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  // createUserWithEmailAndPassword,
-  // signInWithEmailAndPassword,
-  // signOut,
-  // onAuthStateChanged,
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfdtrRhayBtlFeRnoYVO4CMu6JbksrU7A",
@@ -33,3 +35,33 @@ export const signInWithGooglePopup = () =>
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (
+    userAuth,
+    additionalInformation = {}
+  ) => {
+    if (!userAuth) return;
+  
+    const userDocRef = doc(db, 'users', userAuth.uid);
+  
+    const userSnapshot = await getDoc(userDocRef);
+  
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+  
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+          ...additionalInformation,
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message);
+      }
+    }
+  
+    return userDocRef;
+  };
