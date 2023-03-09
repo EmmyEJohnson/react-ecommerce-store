063337bbb26c6d6cdf66bf3cc078900ca06c13e2
Emily Johnson <emelizajo22@gmail.com>
2023-03-09 11:09:48 -0800

    start google redirect sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,13 @@
-import { 
+import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth, 
+  createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  }
+  };
 
   return (
     <div>
