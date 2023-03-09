44608bed0a8a25dcca8507afbba71ff2b943973e
Emily Johnson <emelizajo22@gmail.com>
2023-03-09 15:13:58 -0800

    finish sign up sign in and authentication pages

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,8 +5,15 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc, 
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfdtrRhayBtlFeRnoYVO4CMu6JbksrU7A",
@@ -66,4 +73,10 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
