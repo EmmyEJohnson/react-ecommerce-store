2a2d42092a6e0980fb928edcd3ab2b3c4d6ecb91
Emily Johnson <emelizajo22@gmail.com>
2023-03-09 22:12:33 -0800

    create sign out capabilities

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,9 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
-
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from '../utils/firebase/firebase.utils';
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
@@ -13,17 +8,6 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      setCurrentUser(user);
-    });
-
-    return unsubscribe;
-  }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
