6431417372f8c96148d321409b0f431fcdf7a2bd
Emily Johnson <emelizajo22@gmail.com>
2023-03-13 13:08:28 -0700

    add scss corrections to index.scss file

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+
 import { UserContext } from '../../contexts/user.context';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
