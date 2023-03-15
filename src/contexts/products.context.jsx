10bd07e1921c1e1862850da52997f30bfb5bf5dc
Emily Johnson <emelizajo22@gmail.com>
2023-03-15 13:30:53 -0700

    pull data from firestore in products context file

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,8 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   products: [],
@@ -11,9 +10,14 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
-  // useEffect(() => {
-  //   addCollectionAndDocuments('categories', SHOP_DATA);
-  // }, []);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    }
+    
+    getCategoriesMap();
+  }, []);
 
   const value = { products };
   return (
