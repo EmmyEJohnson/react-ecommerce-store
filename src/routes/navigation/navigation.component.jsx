97a96a2fb77651257fba06021758f153d2e4558f
Emily Johnson <emelizajo22@gmail.com>
2023-03-18 15:20:25 -0700

    convert button files to styled components and fix corresponding files

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,21 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink  } from './navigation.styles';
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from './navigation.styles';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -20,21 +25,17 @@ const Navigation = () => {
     <Fragment>
       <NavigationContainer>
         <LogoContainer to='/'>
-          <CrwnLogo className='logo' />
+          <CrwnLogo />
         </LogoContainer>
         <NavLinks>
-          < NavLink to='/shop'>
-            SHOP
-          </ NavLink>
+          <NavLink to='/shop'>SHOP</NavLink>
 
           {currentUser ? (
-            < NavLink as='span' onClick={signOutUser}>
+            <NavLink as='span' onClick={signOutUser}>
               SIGN OUT
-            </ NavLink>
+            </NavLink>
           ) : (
-            < NavLink to='/auth'>
-              SIGN IN
-            </ NavLink>
+            <NavLink to='/auth'>SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
@@ -45,4 +46,4 @@ const Navigation = () => {
   );
 };
 
-export default Navigation;
+export default Navigation
